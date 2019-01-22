92301c556cbbc025f253135a9d2ce0b970d2169b
Sandrine Caruso <scaruso@invivox.com>
2019-01-22 16:24:48 +0100

    Package corrected

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import accentMap from './accent-map';
 
 function accentRegExp(word) {
   word = word.toLowerCase();
-  let res = '';
-  for (let i = 0; i < word.length; i++) {
+  var res = '';
+  for (var i = 0; i < word.length; i++) {
       const char = word.charAt(i);
       const mapped = accentMap[char];
       if (!mapped) {
@@ -21,6 +21,41 @@ function accentRegExp(word) {
   return new RegExp(res);
 }
 
+function replaceString(word) {
+    var resultats = [''];
+    for (var i = 0; i < word.length; i++) {
+        const char = word.charAt(i);
+        const mapped = accentMap[char];
+        if (!mapped) {
+            resultats.forEach(function(res, index) {
+                resultats[index] = res + char;
+            });
+        } else if (mapped instanceof Array) {
+            var resultats_copy = resultats.slice();
+            resultats.forEach(function(res, index) {
+                resultats[index] = res + char;
+            });
+            for (let j = 0; j < mapped.length; j++) {
+                var resultats_copy_2 = resultats_copy.slice();
+                resultats_copy_2.forEach(function(res, index) {
+                    resultats_copy_2[index] = res + mapped[j];
+                });
+                resultats = resultats.concat(resultats_copy_2);
+            }
+        } else {
+            var resultats_copy = resultats.slice();
+            resultats.forEach((res, index) => {
+                resultats[index] = res + char;
+            });
+            resultats_copy.forEach((res, index) => {
+                resultats_copy[index] = res + mapped;
+            })
+            resultats = resultats.concat(resultats_copy);
+        }
+    }
+    return resultats;
+}
+
 
 function accentInsensitiveSearch(toSearch, searchIn) {
   if (!searchIn) {
@@ -30,9 +65,14 @@ function accentInsensitiveSearch(toSearch, searchIn) {
       return true;
   }
   toSearch = toSearch.replace(/\s+/g, ' ');
-  searchIn = searchIn.replace(/\s+/g, ' ');
+  searchIn = searchIn.replace(/\s+/g, ' ').toLowerCase();
   const re = accentRegExp(toSearch);
-  return re.test(searchIn.toLowerCase());
+  var allStrings = replaceString(searchIn);
+  var res = false;
+  allStrings.forEach((str) => {
+    res = res || re.test(str);
+  });
+  return res;
 }
 
 export default accentInsensitiveSearch;
